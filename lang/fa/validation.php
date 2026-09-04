3783e381781e261a9a547dd2e4e14b094c84e037
mehrabkb <mkordbacheh75@gmail.com>
2026-09-05 01:00:03 +0330

    change login register and dashboard page to persian and rtl

---
+++ lang/fa/validation.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'required' => 'فیلد :attribute الزامی است.',
+
+    'email' => 'فرمت :attribute صحیح نیست.',
+
+    'unique' => ':attribute قبلاً ثبت شده است.',
+
+    'min' => [
+        'string' => 'فیلد :attribute باید حداقل :min کاراکتر باشد.',
+    ],
+
+    'confirmed' => 'تکرار :attribute مطابقت ندارد.',
+
+
+    'attributes' => [
+
+        'name' => 'نام',
+
+        'email' => 'ایمیل',
+
+        'password' => 'رمز عبور',
+
+        'mobile' => 'شماره موبایل',
+
+    ],
+
+];
